6b3d13addc190147b3b236af9e49e8e09c321498
androidifygeek <Professional@123>
2015-10-30 12:23:01 +0530

    Added SlideshowFragment and ToolsFragment

--- app/src/main/java/com/androidify/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/androidify/navigationdrawer/MainActivity.java
@@ -2,9 +2,10 @@ package com.androidify.navigationdrawer;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -74,28 +76,43 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        Fragment fragment = null;
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camara) {
-            // Handle the camera action
+            fragment = new ImportActivityFragment();
+
         } else if (id == R.id.nav_gallery) {
-
+            fragment = new GalleryFragment();
         } else if (id == R.id.nav_slideshow) {
-
+            fragment = new SlideshowFragment();
         } else if (id == R.id.nav_manage) {
-
+            fragment = new ToolsFragment();
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
         }
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
 
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
