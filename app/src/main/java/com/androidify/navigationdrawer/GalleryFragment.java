39018b3fb569887c88658f16d3a7777ee574a566
androidifygeek <Professional@123>
2015-10-30 12:49:04 +0530

    GalleryFragment

---
+++ app/src/main/java/com/androidify/navigationdrawer/GalleryFragment.java
@@ -0,0 +1,23 @@
+package com.androidify.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class GalleryFragment extends Fragment {
+    public GalleryFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+
+}
