567836e65a6cba503dfd24ff6634589ac0bd83bc
androidifygeek <Professional@123>
2015-10-30 12:48:53 +0530

    ToolsFragment

---
+++ app/src/main/java/com/androidify/navigationdrawer/ToolsFragment.java
@@ -0,0 +1,25 @@
+package com.androidify.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ToolsFragment extends Fragment {
+
+
+
+    public ToolsFragment(){
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tools, container, false);
+    }
+
+
+}
