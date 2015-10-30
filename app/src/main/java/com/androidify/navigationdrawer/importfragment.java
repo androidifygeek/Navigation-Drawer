1e97d551bee23783eec2eb35e83b57057348c05e
androidifygeek <Professional@123>
2015-10-30 12:49:19 +0530

    ImportFragment

---
+++ app/src/main/java/com/androidify/navigationdrawer/importfragment.java
@@ -0,0 +1,20 @@
+package com.androidify.navigationdrawer;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class importfragment extends Fragment {
+
+    public importfragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_import, container, false);
+    }
+}
