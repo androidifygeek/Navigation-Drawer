f65c570963a2164663c7fa73d891db4a003ce966
androidifygeek <Professional@123>
2015-10-30 12:48:40 +0530

    Slideshow Fragment

---
+++ app/src/main/java/com/androidify/navigationdrawer/SlideshowFragment.java
@@ -0,0 +1,21 @@
+package com.androidify.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SlideshowFragment extends Fragment {
+
+    public SlideshowFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_slideshow, container, false);
+    }
+}
